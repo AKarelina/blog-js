57ac073ccacf86199bf84d837d93162d0c51c4d4
AKarelina <annagjindj@gmail.com>
2023-02-13 18:14:19 -0500

    Add dialogs component CSS

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import d from './Dialogs.module.css';
 
 export const Dialogs = (props) => {
     return (
-        <div className={d.content}>
-            Dialogs
+        <div className={d.dialogs}>
+            <div className={d.dialogsItems}>
+                <div className={d.dialog}>
+                    Anna
+                </div>
+                <div className={d.dialog}>
+                   Valeria
+                </div>
+                <div className={d.dialog}>
+                    David
+                </div>
+            </div>
+            <div className={d.messages}>
+                <div className={d.message}>Hi!</div>
+                <div className={d.message}>I am Anna!</div>
+                <div className={d.message}>Hi, How are you?!</div>
+                <div className={d.message}>Hello!</div>
+            </div>
+
         </div>
+
     )
 }
