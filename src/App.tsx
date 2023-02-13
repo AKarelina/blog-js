cb7b7e7092e6ed8b9fbdbf4859182416094aea19
AKarelina <annagjindj@gmail.com>
2023-02-13 12:40:55 -0500

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,31 @@ import {Header} from "./components/Header/Header";
 import {Profile} from "./components/Profile/Profile"
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {Navbar} from "./components/Navbar/Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Settings} from "./components/Settings/Settings";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
 
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
-            </div>
-            {/*<Profile/>*/}
-        </div>
+        <BrowserRouter>
+                <div className="app-wrapper">
+                    <Header/>
+                    <Navbar/>
+                    <div className='app-wrapper-content'>
+                        <Routes>
+                            <Route path="/profile" element={<Profile/>} />
+                            <Route path="/dialogs" element={<Dialogs/>}/>
+                            <Route path="/news" element={<News/>}/>
+                            <Route path="/music" element={<Music/>}/>
+                            <Route path="/settings" element={<Settings/>}/>
+                        </Routes>
+                    </div>
+                </div>
+
+        </BrowserRouter>
+
     );
 }
 
